912a36c015e37b741df8d8cab8c545971ddcd325
Saad <saad.ziauddin@pronet-tech.net>
2024-09-05 00:06:26 +0500

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewConsoleApp1
+{
+    internal class MyClass
+    {
+        //public MyClass()
+        //{
+        //    Console.WriteLine("The Auto Fired Constructor From MyCLass!");
+        //}
+
+        public void table()
+        {
+            Console.WriteLine("Customised Table!");
+
+            int Table, Start, End;
+
+            Console.WriteLine("Enter number whose table you want: ");
+            Table = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter starting number: ");
+            Start = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter ending number: ");
+            End = int.Parse(Console.ReadLine());
+
+            for(int i = Start; i <= End; i++)
+            {
+                Console.WriteLine(Table + " x " + i + " = " + (i * Table));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using NewConsoleApp1;
+
+Console.WriteLine("Welcome to C#");
+
+MyClass c1 = new MyClass();
+c1.table();
